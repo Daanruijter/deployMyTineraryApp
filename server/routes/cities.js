b6b3b480fbe83a2f359425831913f88ec7f70190
Daan Ruijter <daanruijter@gmail.com>
2020-02-03 15:26:41 +0100

    connection to MongoDD established and able to GET data from it

--- server/routes/cities.js
+++ server/routes/cities.js
@@ -1,8 +1,42 @@
 
+
+
 const express = require('express')
 
-
 const router = express.Router()
+
+const cityModel = require('../model/cityModel')
+
+
+router.get('/testing',function(req,res){
+res.send({type:'get'})
+
+})
+
+
+
+/*get all cities*/
+router.get('/all',
+    (req, res) => {
+        console.log(res +"RESSSSSSSSSSSSSSSS")
+        cityModel.find({})
+            .then(files => {
+                res.send(files)
+            })
+            .catch(err => console.log(err));
+    });
+
+
+    router.get('/one',
+    (req, res) => {
+        console.log(res +"RESSSSSSSSSSSSSSSS")
+        cityModel.findById('5e3827091c9d4400006098dc')
+            .then(files => {
+                res.send(files)
+            })
+            .catch(err => console.log(err));
+    });
+
 
 router.get('/test', (req, res) => {
     
--- server/server.js
+++ server/server.js
@@ -1,77 +1,35 @@
-//try to connect with MongoDB//
-const express =  require("express")
-const app = express();
+
 
 const db = require('./keys').mongoURI;
 const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
- mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true })
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("Server is running on " + port + "port");
+});
+
+app.use('/cities', require('./routes/cities'))
+
+ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, dbName: 'itinerary-app' })
      .then(() => console.log('Connection to Mongo DB established'))
-     .catch(err => console.log(err + "scheisse, de verbinding IS ER NIET!!!"));
-
-
-// mongoose.connect('mongodb://localhost/testaroo');
+     .catch(err => console.log(err + "There is no connection"));
 
 
 
 
 
 
-// mongoose.connection.once('open', function() {
-//   console.log("connection had been made")
-// })
-// .on('error', function (error){
-// console.log(error +   "ERRORRRRRRR")
-// })
 
-
-
-
-
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-
-// const db = require('./keys').mongoURI;
-// const mongoose = require("mongoose");
-// console.log(db + "ASFDSLSGJSLGGLFSS")
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true })
-//     .then(() => console.log('Connection to Mongo DB established'))
-//     .catch(err => console.log(err + "scheisse, de verbinding IS ER NIET!!!"));
-
-
-// const MongoClient = require('mongodb').MongoClient
-//     var ObjectID = require('mongodb').ObjectID; // we will use this later
-    
-
-// MongoClient.connect('mongodb://dr:2CWRERL6fHjet7kf@ds247027.mlab.com:47027/itinerary-app', (err, db) => {
-//   console.log(db +"dbbbbbb")
-//   var dbase = db.db("itinerary-app");
-//   if (err) return console.log(err + ADFDFADAD)
-//   app.listen(port, () => {
-//     console.log('app working on 3000')
-//   })
-// })
-
-
-
-
-// app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true
-//   })
-// );
-// app.use(cors());
-
-
-// app.use('/cities', require('./routes/cities'))
-
-
-
-
-
-// mongodb+srv://dr:<password>@cluster0-4ie2c.mongodb.net/test?retryWrites=true&w=majority
