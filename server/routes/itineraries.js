30b2e6599fa407a16c7d0967600c028819047abe
Daan Ruijter <daanruijter@gmail.com>
2020-02-14 12:59:03 +0100

    working on itineraries

--- server/routes/itineraries.js
+++ server/routes/itineraries.js
@@ -16,7 +16,59 @@ router.get('/routetjes', (req, res) => {
 
 
 /*get all itineraries*/
-router.get('/rou', (req, res) => {
+router.get('/Amsterdam', (req, res) => {
+    console.log(res +"RESSSSSSSSSSSSSSSS")
+    // let cityRequested = req.params.parentCity_id;
+    // console.log(cityRequested)
+    itineraryModel.find({ parentCity_id : "5e3940de1c9d4400002af04b" })
+    // console.log(parentCity_id)
+        .then(files => {
+            console.log(files)
+            return res.send(files)
+        })
+        .catch(err => console.log(err));
+});
+
+router.get('/Valencia', (req, res) => {
+    console.log(res +"RESSSSSSSSSSSSSSSS")
+    // let cityRequested = req.params.parentCity_id;
+    // console.log(cityRequested)
+    itineraryModel.find({ parentCity_id : "5e3940c01c9d4400002af04a" })
+    // console.log(parentCity_id)
+        .then(files => {
+            console.log(files)
+            return res.send(files)
+        })
+        .catch(err => console.log(err));
+});
+
+router.get('/Malaga', (req, res) => {
+    console.log(res +"RESSSSSSSSSSSSSSSS")
+    // let cityRequested = req.params.parentCity_id;
+    // console.log(cityRequested)
+    itineraryModel.find({ parentCity_id : "5e3940891c9d4400002af049" })
+    // console.log(parentCity_id)
+        .then(files => {
+            console.log(files)
+            return res.send(files)
+        })
+        .catch(err => console.log(err));
+});
+
+router.get('/Barcelone', (req, res) => {
+    console.log(res +"RESSSSSSSSSSSSSSSS")
+    // let cityRequested = req.params.parentCity_id;
+    // console.log(cityRequested)
+    itineraryModel.find({ parentCity_id : "5e3827931c9d4400006098dd" })
+    // console.log(parentCity_id)
+        .then(files => {
+            console.log(files)
+            return res.send(files)
+        })
+        .catch(err => console.log(err));
+});
+
+router.get('/Paris', (req, res) => {
     console.log(res +"RESSSSSSSSSSSSSSSS")
     // let cityRequested = req.params.parentCity_id;
     // console.log(cityRequested)
@@ -31,4 +83,20 @@ router.get('/rou', (req, res) => {
 
 
 
+
 module.exports = router
+
+//Amsterdam 
+// 5e4521041c9d4400003cc9eb
+
+// Valencia
+// 5e3940c01c9d4400002af04a
+
+// Malaga
+// 5e3940891c9d4400002af049
+
+// Barcelone
+// 5e3827931c9d4400006098dd
+
+// Paris
+// 5e3940de1c9d4400002af04b
