fdf945eef7938ca2b141037146573c75558391ec
Daan Ruijter <daanruijter@gmail.com>
2020-02-17 13:47:34 +0100

    changed the backend to get the city itineraries based on the citiy users click

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
 
+
+//if you are on this path, require this file//
 app.use('/cities', require('./routes/cities'))
 app.use('/itineraries', require('./routes/itineraries'))
 
