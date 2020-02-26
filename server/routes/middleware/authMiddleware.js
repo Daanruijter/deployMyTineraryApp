f63ce62fd9179c371f3c90bee6573a782e9c2250
Daan Ruijter <daan_ruijter@hotmail.com>
2020-02-26 17:43:11 +0100

    minor change on the middleware auth function

--- server/routes/middleware/authMiddleware.js
+++ server/routes/middleware/authMiddleware.js
@@ -1,34 +1,30 @@
-const config = require('config')
-const jwt = require('jsonwebtoken')
-console.log("akdhakdhjadkhd")
+const config = require("config");
+const jwt = require("jsonwebtoken");
+console.log("akdhakdhjadkhd");
 
 //next brings you to the next middleware once you are done with what the middleware does//
 
 //function sends along a token//
-function auth (req,res,next){
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
 
-    const token = req.header('x-auth-token')
-    
-console.log("akdhakdhjadkhd")
-    console.log(token)
-    //check for token//
-    if(!token){
-        //401=you don't have the right permission: user is unauthorized//
-        res.status(401).json({msg:'No token, authorization denied'})
-       
-    }
+  console.log("akdhakdhjadkhd");
+  console.log(token);
+  //check for token//
+  if (!token) {
+    //401=you don't have the right permission: user is unauthorized//
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-    try {
- //verify token//
- const decoded = jwt.verify (token, config.get('jwtSecret'))
- //add user from payload//
- req.user=decoded
- next()
-    }
-    catch(e){
-        res.status(400).json({msg:'Token is not valid'})
-    }
-  
+  try {
+    //verify token//
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    //add user from payload//
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
 }
 
-module.exports = auth
+module.exports = auth;
