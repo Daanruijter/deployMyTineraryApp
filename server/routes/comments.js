f3c786f12a3b7a97fd69b58318beb701f0cb7dd1
Daan Ruijter <daan_ruijter@hotmail.com>
2020-03-26 16:19:59 +0100

    working on comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -10,8 +10,23 @@ const auth = require("./middleware/authMiddleware");
 
 //get the current user with the token
 
-router.post("/", (req, res) => {
-  //   console.log(req);
+router.post("/saveComment", (req, res) => {
+  const comment = new commentModel(req.body);
+  comment.save((err, comment) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    console.log(comment._id);
+    // commentModel
+    //   .find({ id: comment._id })
+    //   .populate("writer")
+    //   .exec((err, result) => {
+    //     if (err) {
+    //       return res.json({ success: false, err });
+    //     }
+    //     return res.status(200).json({ succes: true, result });
+    //   });
+  });
 });
 
 module.exports = router;
